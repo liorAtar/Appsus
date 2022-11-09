9d290386b2059812c7935543cefe5557e8ec25a2
LielR <lielr10@gmail.com>
2022-11-09 11:32:17 +0200

    added services to note app

--- apps/keep/services/note.service.js
+++ apps/keep/services/note.service.js
@@ -6,17 +6,53 @@ export const noteService = {
   query,
   remove,
   get,
+  updateTodo,
+  deleteTodo,
+  addNewNote,
+  updateNote,
 }
+function updateNote(note) {
+  storageService.put(KEEP_NOTE_KEY, note)
+}
+
 function query() {
   return storageService.query(KEEP_NOTE_KEY)
 }
+
 function remove(noteId) {
   return storageService.remove(KEEP_NOTE_KEY, noteId)
+}
+
+function updateTodo(note, todoUpdate) {
+  let index = todoUpdate.index
+  note.info.todos[index] = todoUpdate
+  console.log('index', index);
+  storageService.put(KEEP_NOTE_KEY, note)
+}
+
+function deleteTodo(note, deleteTodo) {
+  let index = deleteTodo.index
+  note.info.todos.splice(index, 1)
+  for (var i = 0; i < note.info.todos.length; i++) {
+    note.info.todos[i].index = i
+  }
+  updateNote(note)
 }
 
 function get(noteId) {
   return storageService.get(KEEP_NOTE_KEY, noteId)
 }
+
+function checkNotes(notes) {
+  console.log(notes)
+  if (!notes || !notes.length) {
+    console.log('fetching notes - ', notes, notes.length)
+    notes = getNotes()
+    storageService._save(KEEP_NOTE_KEY, notes)
+  }
+  return notes
+}
+
 function _createNotes() {
   storageService.query(KEEP_NOTE_KEY).then()
 }
