5be9afc75978a7cdd2866968e9b37143e2bc0db0
Lior Atar <Liorat18@gmail.com>
2022-11-12 18:57:27 +0200

    link by github

--- cmps/app-header.cmp.js
+++ cmps/app-header.cmp.js
@@ -4,7 +4,7 @@ export default {
         <router-link to="/"> <img :src="imgUrl" /> </router-link> 
             <nav>
                 <router-link to="/keep">Keep </router-link>
-                <router-link to="/mail/inbox">Mail </router-link>
+                <router-link to="/Appsus/mail/inbox">Mail </router-link>
                 <router-link to="/about">About </router-link>
             </nav>
         </header>
